719e33aef41268a07a7b56a924d924981b38b166
CRGBS <32331418+CRGBS@users.noreply.github.com>
2024-12-12 11:23:17 +0800

    Update rest.bat

--- rest.bat
+++ rest.bat
@@ -1,9 +1,9 @@
-@echo off
+@echo on
 setlocal enabledelayedexpansion
 
 rem Initialize timer duration
 set seconds=1800
-set title=30-minute work Timer
+set title=30-minute Timer
 
 :timer
 rem Set window title
@@ -11,33 +11,24 @@ title %title%
 
 rem Start the timer
 for /l %%i in (1,1,%seconds%) do (
-    timeout /t 1 >nul
     set /a remaining=%seconds%-%%i
     set /a minutes=remaining/60
     set /a secs=remaining%%60
-	cls
+    cls
     echo Remaining time: !minutes! minutes !secs! seconds
-    echo Press R to reset the timer, 1 for 10 minutes rest, 2 for 30 minutes work
-    set "input="
-    set /p "input=Enter command: " <nul
-    if /i "!input!"=="R" goto reset
-    if /i "!input!"=="1" goto set10
-    if /i "!input!"=="2" goto set30
+    timeout /t 1 /nobreak >nul
 )
 
-echo Timer finished!
-pause
-goto timer
-
-:set10
-set seconds=600
-set title=10-minute rest Timer
-goto timer
-
-:set30
-set seconds=1800
-set title=30-minute work Timer
-goto timer
+if "%title%"=="30-minute Timer" (
+    set seconds=600
+) else (
+    set seconds=1800
+)
 
 :reset
+if "%title%"=="30-minute Timer" (
+    set seconds=1800
+) else (
+    set seconds=600
+)
 goto timer
